0345add783a577632150fc7603ecc08db01bb4ec
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2017-10-24 22:27:14 +0200

    Mechanizm admin/user

---
+++ website/VotingSystem/resources/views/auth/register.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.app')
+
+@section('content')
+<script>
+$(function() {
+    $('#root').click(function() {
+        if ($(this).is(':checked')) {
+            $('#role').removeAttr('disabled');
+        } else {
+            $('#role').attr('disabled', 'disabled');
+        }
+    });
+});
+</script>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+                         <div class="form-group">
+                            <label for="admin" class="col-md-4 control-label">Admin</label> 
+
+                            <div class="col-md-6">
+                                <input type="checkbox" id = "root" name="root" />
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="rola" class="col-md-4 control-label">Rola</label> 
+
+                            <div class="col-md-6">
+                                <input id="role" type="password" class="form-control" name="role" disabled="" />
+                            </div>
+                        </div>
+                                                        
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
